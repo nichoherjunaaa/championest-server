a1b136670445f75a5fa6733930ec0a2203acfb56
nichoherjunaa <nichoherjunaa@gmail.com>
2025-02-15 22:30:40 +0700

    create add docu and get img

--- controller/dokumentasiController.js
+++ controller/dokumentasiController.js
@@ -2,6 +2,7 @@ import Dokumentasi from '../models/dokumentasiModel.js';
 import asyncHandler from 'express-async-handler';
 import streamifier from 'streamifier';
 import { v2 as cloudinary } from 'cloudinary'
+
 export const getDokumentasi = asyncHandler(async (req, res) => {
     const dokumentasi = await Dokumentasi.find();
     res.status(200).json({
@@ -10,7 +11,7 @@ export const getDokumentasi = asyncHandler(async (req, res) => {
     });
 });
 
-export const addDokumentasi = asyncHandler(async (req, res) => {
+export const uploadDokumentasi = asyncHandler(async (req, res) => {
     const stream = cloudinary.uploader.upload_stream({
         folder: "docu",
         allowed_formats: ['jpg', 'png', 'jpeg'],
@@ -28,3 +29,15 @@ export const addDokumentasi = asyncHandler(async (req, res) => {
     })
     streamifier.createReadStream(req.file.buffer).pipe(stream);
 });
+
+export const addDokumentasi = asyncHandler(async (req, res) => {
+    const { gambar } = req.body;
+    const dokumentasi = new Dokumentasi({
+        gambar
+    })
+    dokumentasi.save()
+    res.status(201).json({
+        status: 'success',
+        data: dokumentasi
+    })
+})
--- routes/dokumentasiRoute.js
+++ routes/dokumentasiRoute.js
@@ -1,10 +1,11 @@
-import { getDokumentasi, addDokumentasi } from "../controller/dokumentasiController.js";
+import { getDokumentasi, addDokumentasi, uploadDokumentasi } from "../controller/dokumentasiController.js";
 import express from "express";
 import { upload } from './../utils/uploadFile.js';
 import { protectedMiddleware, isAdminMiddleware } from './../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.get('/', getDokumentasi);
-router.post('/', protectedMiddleware, isAdminMiddleware, upload.single('gambar'), addDokumentasi);
+router.post('/', protectedMiddleware, isAdminMiddleware, upload.single('gambar'), uploadDokumentasi);
+router.post('/add', protectedMiddleware, isAdminMiddleware, addDokumentasi);
 
 export default router;
